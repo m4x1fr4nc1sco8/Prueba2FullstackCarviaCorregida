adaa3d202701ed4c8cfc0425aad3c8362d8efc74
Maximiliano <ma.araosf@duocuc.cl>
2026-06-19 23:08:27 -0400

    commit openapiconfig

---
+++ pago-service/src/main/java/cl/duoc/pago_service/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package cl.duoc.pago_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server gatewayServer = new Server();
+        gatewayServer.setUrl("http://localhost:9090");
+        gatewayServer.setDescription("API Gateway");
+
+        return new OpenAPI()
+                .servers(List.of(gatewayServer));
+    }
+}
